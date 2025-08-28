711abe98d0ee659a687f5a63fab27d47a70bab8c
linoliumplitkoviy-ux <linoliumplitkoviy@gmail.com>
2025-08-29 00:40:22 +0300

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -13,7 +13,7 @@ local EEPROMAddress, internetAddress, GPUAddress =
 component.invoke(GPUAddress, "bind", getComponentAddress("screen"))
 local screenWidth, screenHeight = component.invoke(GPUAddress, "getResolution")
 
-local repositoryURL = "https://raw.githubusercontent.com/linoliumplitkoviy-ux/literate-computing-machine/refs/heads/main/master/"
+local repositoryURL = "https://raw.githubusercontent.com/linoliumplitkoviy-ux/literate-computing-machine/refs/heads/main/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
